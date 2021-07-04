001651ae2cfc023057b6b2ba10f77088dc7066c1
— Λsᴡɪɴ Rᴀᴊ — <82755354+Aswin-TG@users.noreply.github.com>
2021-07-04 15:50:48 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -230,8 +230,14 @@ async def auto_filter(bot, update):
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
+
+           await asyncio.sleep(250)
+           await update.message.delete()
+
            await asyncio.sleep(300)
            await wafikh.delete()
+
+           
            
         except ButtonDataInvalid:
             print(result[0])
