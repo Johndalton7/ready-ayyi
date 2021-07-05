b56d589d9372cb3b4fc06cedc8074717e4070bac
Professor <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-07-05 18:50:39 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -230,7 +230,7 @@ async def auto_filter(bot, update):
                 parse_mode="html",
                 reply_to_message_id=update.message_id
            )
-           await asyncio.sleep(5)
+           await asyncio.sleep(300)
            await wafikh.delete()
 
            
