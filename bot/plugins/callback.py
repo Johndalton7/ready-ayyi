d4bc8206e8336d552261f52a2cb10619345db7fb
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-07-09 17:57:03 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -81,7 +81,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if ((index_val + 1 )== max_pages) or ((index_val + 1) == len(results)): # Max Pages
         temp_results.append([
-            InlineKeyboardButton("<< ʙᴀᴄᴋ", callback_data=f"navigate({index_val}|back|{query})")
+            InlineKeyboardButton("<< 𝘽𝘼𝘾𝙆", callback_data=f"navigate({index_val}|back|{query})")
         ])
 
     elif int(index_val) == 0:
